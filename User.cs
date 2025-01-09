ba5206b2ef22839cb7c84b94a7a39e7e7faacda8
Cristian-Alexandru Pană <cristian.pana@student.upt.ro>
2025-01-09 20:24:57 +0200

    Adaugat comentarii pentru functiile planificate, trasatura de shopping cart pentru client

--- User.cs
+++ User.cs
@@ -19,6 +19,7 @@ public abstract class User
         Password = password;
         EmailAddress = emailAddress;
         UserType = userType;
+        
     }
     
     public bool VerifyUserCredentials(string emailToVerify, string passwordToVerify)
@@ -30,9 +31,12 @@ public abstract class User
 
 public class Client : User
 {
+    public Dictionary<Produs, int> ShoppingCart { get; protected set; }
+
     public Client(string firstName, string lastName, string password, string emailAddress)
         : base(firstName, lastName, password, emailAddress, UserTypes.Client)
     {
+        ShoppingCart = new Dictionary<Produs, int>();
     }
 
     public void RunMenu()
@@ -44,9 +48,13 @@ public class Client : User
             Console.WriteLine("Meniu Client:");
             Console.WriteLine("1. Vizualizeaza produse");
             Console.WriteLine("2. Adauga in cos");
-            Console.WriteLine("3. Finalizeaza comanda");
-            Console.WriteLine("4. Vizualizeaza wishlist");
-            Console.WriteLine("5. Iesi");
+            Console.WriteLine("3. Modifica cosul");
+            Console.WriteLine("4. Finalizeaza comanda");
+            Console.WriteLine("5. Anuleaza o comanda");
+            Console.WriteLine("6. Adauga in wishlist");
+            Console.WriteLine("7. Afiseaza wishlist-ul");
+            Console.WriteLine("8. Adauga rating unui produs");
+            Console.WriteLine("9. Iesi");
             Console.Write("Alegeti optiunea: ");
             var input = Console.ReadLine();
 
@@ -54,17 +62,40 @@ public class Client : User
             {
                 case "1":
                     // ViewProducts();
+                    //
+                    // listeaza toate produsele, cu un contor care le numeroteaza in afisaj
+                    // pentru a le putea selecta
                     break;
                 case "2":
                     // AddToCart();
                     break;
                 case "3":
-                    // FinalizeOrder();
+                    // EditCart();
                     break;
                 case "4":
-                    // ViewWishlist();
+                    // FinalizeOrder();
+                    //
+                    // Creaza o comanda cu toate produsele din cos
                     break;
                 case "5":
+                    // CancelOrder();
+                    break;
+                case "6":
+                    // AddToWishlist();
+                    break;
+                case "7":
+                    // ViewWishlist();
+                    //
+                    // din wishlist produsele trebuie sa se poata adauga in cart
+                    break;
+                case "8":
+                    // RateProduct();
+                    //
+                    // valabil doar pentru produsele care au fost in comenzi asociate clientului
+                    // poate le listam si cerem alegerea pentru ce produs sa fie rate-uit?
+                    break;
+                case "9":
+                    // Exit
                     running = false;
                     break;
                 default:
@@ -90,33 +121,38 @@ public class Admin : User
         {
             Console.Clear();
             Console.WriteLine("Meniu Admin:");
-            Console.WriteLine("1. Adauga produs");
-            Console.WriteLine("2. Modifica produs");
-            Console.WriteLine("3. Sterge produs");
-            Console.WriteLine("4. Vizualizeaza comenzi");
-            Console.WriteLine("5. Genereaza raport de vanzari");
-            Console.WriteLine("6. Iesi");
+            Console.WriteLine("1. Administrare magazin");
+            Console.WriteLine("2. Administrare comenzi");
+            Console.WriteLine("3. Genereaza raport de vanzari");
+            Console.WriteLine("4. Iesi");
             Console.Write("Alegeti optiunea: ");
             string input = Console.ReadLine();
 
             switch (input)
             {
                 case "1":
-                    // AddProduct();
+                    // ManageStore();
+                    // 
+                    // functie multifunctionala pentru adaugat, editat si sters produse
+                    // inclusiv administrat stocurile
+                    // produsele cu stoc redus ar trebui semnalate cumva
+                    // adaugatul de discounturi probabil vine tot aici
                     break;
                 case "2":
-                    // EditProduct();
+                    // ManageOrders();
+                    //
+                    // pentru vizualizat, editat si modificat statusul comenzilor
                     break;
                 case "3":
-                    // DeleteProduct();
+                    // CreateSalesReport();
+                    //
+                    // asta ar trebui sa ofere statistici
+                    // precum sa treaca prin toate comenzile existente care nu au fost anulate
+                    // si sa arate cele mai cumparate produse
+                    // sau cifra totala de venituri
                     break;
                 case "4":
-                    throw new NotImplementedException();
-                    break;
-                case "5":
-                    throw new NotImplementedException();
-                    break;
-                case "6":
+                    // Exit
                     running = false;
                     break;
                 default:
